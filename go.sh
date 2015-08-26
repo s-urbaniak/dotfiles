9a7948903442745c7d02f17d5c55ece93b8346c1
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-08-26 15:02:00 +0200

    add gorename in go installer

--- go.sh
+++ go.sh
@@ -6,8 +6,9 @@ $goget github.com/rogpeppe/godef
 $goget github.com/nsf/gocode
 $goget github.com/s-urbaniak/agoc
 $goget github.com/s-urbaniak/acme/cmd/apl
+$goget 9fans.net/go/acme/acmego
 
-for tool in "oracle" "goimports" "present" "godoc" "vet" "eg"; do
+for tool in "gorename" "oracle" "goimports" "present" "godoc" "vet" "eg"; do
     $goget "golang.org/x/tools/cmd/$tool"
 done
 
